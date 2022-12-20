eb622fa60d21ef257f0882287eb86b7efb26de1b
ShilpsWaikar <shilpa.waikar@aressindia.net>
2022-12-20 13:56:27 +0530

    Recruitment Page

---
+++ framework/src/main/java/webelement/RecruitmentPage.java
@@ -0,0 +1,26 @@
+package webelement;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class RecruitmentPage
+{
+	@FindBy(xpath="//a[@href='/web/index.php/recruitment/viewRecruitmentModule']")
+	WebElement recruitment;
+	
+	@FindBy(xpath="//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[1]/div/div[1]/div/div[2]/div/div")
+	WebElement JobTitle;
+	
+	@FindBy(className="oxd-autocomplete-text-input oxd-autocomplete-text-input--active")
+	WebElement candidateName;
+	
+	@FindBy(xpath="//input[@placeholder='Enter comma seperated words...']")
+	WebElement keywords;
+	
+	@FindBy(xpath="//button[@type='submit']")
+	WebElement search;
+	
+	@FindBy(xpath="//button[@type='reset']")
+	WebElement reset;
+
+}
