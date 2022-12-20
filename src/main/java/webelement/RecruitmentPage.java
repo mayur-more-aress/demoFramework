91c4c6c33bbb9ae8f89b556783ecd8b4266f5276
ShilpsWaikar <shilpa.waikar@aressindia.net>
2022-12-20 16:56:22 +0530

    modified files

--- src/main/java/webelement/RecruitmentPage.java
+++ src/main/java/webelement/RecruitmentPage.java
@@ -5,28 +5,31 @@ import org.openqa.selenium.support.FindBy;
 
 import TestElement.TestBase;
 
-public class RecruitmentPage extends TestBase
-{
-	@FindBy(xpath="//a[@href='/web/index.php/recruitment/viewRecruitmentModule']")
+public class RecruitmentPage extends TestBase {
+	@FindBy(xpath = "//a[@href='/web/index.php/recruitment/viewRecruitmentModule']")
 	WebElement recruitment;
-	
-	@FindBy(xpath="//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[1]/div/div[1]/div/div[2]/div/div")
+
+	@FindBy(xpath = "//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[1]/div/div[1]/div/div[2]/div/div")
 	WebElement JobTitle;
-	
-	@FindBy(className="oxd-autocomplete-text-input oxd-autocomplete-text-input--active")
+
+	@FindBy(className = "oxd-autocomplete-text-input oxd-autocomplete-text-input--active")
 	WebElement candidateName;
-	
-	@FindBy(xpath="//input[@placeholder='Enter comma seperated words...']")
+
+	@FindBy(xpath = "//input[@placeholder='Enter comma seperated words...']")
 	WebElement keywords;
-	
-	@FindBy(xpath="//button[@type='submit']")
+
+	@FindBy(xpath = "//button[@type='submit']")
 	WebElement search;
-	
-	@FindBy(xpath="//button[@type='reset']")
+
+	@FindBy(xpath = "//button[@type='reset']")
 	WebElement reset;
-}
+
 
 public void recruitmentpage()
 {
 	recruitment.click();
+}
+
+
+
 }
--- src/test/java/testcases/LoginPageTest.java
+++ src/test/java/testcases/LoginPageTest.java
@@ -1,4 +1,4 @@
-package framework;
+package testcases;
 
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
